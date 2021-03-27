dd7a366ef0f053b4492d02d60e0ea9465813c903
Mateusz Brzezinski <mateusz0brzezinski@gmail.com>
2021-03-27 19:09:24 +0100

    get product data from firebase in shop page

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
-import { ProductsContext } from '../contexts/ProductsContext';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { pageAnimation } from '../animation';
+
+import { ref } from "../firebase";
 
 import Product from '../components/Product';
 import Filter from '../components/Filter';
@@ -10,12 +11,35 @@ import Grid from '@material-ui/core/Grid';
 import styled from "styled-components";
 
 const ShopPage = props => {
-    const [products] = useContext(ProductsContext);
+
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState([])
+
+    const getProducts = () => {
+        setLoading(true);
+        ref.onSnapshot((querySnapshot) => {
+            const items = [];
+            querySnapshot.forEach((doc) => {
+                items.push(doc.data());
+            })
+            setProducts(items);
+            setLoading(false);
+            console.log(items)
+        })
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, [])
+
+    if (loading) {
+        return <h2>Loading...</h2>
+    }
 
     const renderedProducts = products
         .filter(product => (product.sex === props.value
             || (product.sex === null && props.value !== 'onSale'))
-            || (props.value === 'onSale' && product.isOnSale === true))
+            || (props.value === 'onSale' && product.isOnsale === true))
         .map(product => (
             <Product
                 key={product.id}
@@ -25,7 +49,7 @@ const ShopPage = props => {
                 price={product.price}
                 sale={product.sale}
                 imgUrl={product.imgUrl}
-                isOnSale={product.isOnSale}
+                isOnSale={product.isOnsale}
                 isNew={product.isNew}
             />
         ));
