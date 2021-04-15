7408ce37a7d299d25d2e7de8caeb821620f3cbbd
Mateusz Brzeziski <mateusz0brzezinski@gmail.com>
2021-04-15 20:17:48 +0200

    add burger menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { NavLink } from "react-router-dom";
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import { CartContext } from '../contexts/CartContext';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import styled from "styled-components";
 
@@ -9,19 +10,33 @@ import logo from "../img/b_logo.svg";
 
 const Menu = props => {
     const [cart, setInCart] = useContext(CartContext);
+    const [isOpen, setIsOpen] = useState(false);
 
     let cartNumber = cart.length;
+
+    const burgerHandler = () => {
+        setIsOpen(current => !current)
+    }
 
     return (
         <Nav>
             <div className="wrap">
-                <NavLink exact to='/'><img className="logo" src={logo} alt="logo" /></NavLink>
-                <div className="tabs">
-                    <NavLink exact to='/shop/sale'>Sale</NavLink>
-                    <NavLink to='/shop/men'>Men</NavLink>
-                    <NavLink to='/shop/women'>Women</NavLink>
-                    <NavLink to='/contact'>Contact</NavLink>
-                    <NavLink to='/cart'>
+                <div className="menu-tab">
+                    <NavLink exact to='/'>
+                        <img className="logo"
+                            src={logo} alt="logo" />
+                    </NavLink>
+                    <Burger onClick={burgerHandler} >
+                        <MenuIcon />
+                    </Burger>
+                </div>
+
+                <div className={isOpen ? 'tabs tabs-open' : 'tabs'}>
+                    <NavLink onClick={burgerHandler} exact to='/shop/sale'>Sale</NavLink>
+                    <NavLink onClick={burgerHandler} to='/shop/men'>Men</NavLink>
+                    <NavLink onClick={burgerHandler} to='/shop/women'>Women</NavLink>
+                    <NavLink onClick={burgerHandler} to='/contact'>Contact</NavLink>
+                    <NavLink onClick={burgerHandler} to='/cart'>
                         <ShoppingCartOutlinedIcon
                             style={
                                 cartNumber === 0 ?
@@ -37,6 +52,14 @@ const Menu = props => {
     )
 }
 
+const Burger = styled.div`
+width: 2rem;
+font-size: 2.4rem;
+margin-right: 1rem;
+@media (min-width: 768px) {
+display: none;
+}
+`
 const Nav = styled.nav`
 width: 100vw;
 height: 3rem;
@@ -50,11 +73,25 @@ box-shadow: 0px 0px 7px rgba(0, 0, 0, 0.5);
     margin: 0 auto;
     display: flex;
     justify-content: space-between;
+    @media (max-width: 768px) {
+        flex-direction: column;
+      }
+}
+
+.menu-tab{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 }
 
 .logo{
     width: 18rem;
     height: 3rem;
+}
+.tabs-open{
+    @media (max-width: 768px) {
+    transform: translateX(0) !important;
+}
 }
 .tabs{
     display: flex;
@@ -63,6 +100,18 @@ box-shadow: 0px 0px 7px rgba(0, 0, 0, 0.5);
     align-items: center;
     height: 3rem;
     flex-wrap: nowrap;
+    @media (max-width: 768px) {
+        transform: translateX(-100vw);
+        flex-direction: column;
+        height: 100vh;
+        margin-top: -5px;
+        width: 100vw;
+        background: #fff;
+        justify-content: space-around;
+        font-size: 1.3rem;
+        z-index: 99;
+        transition: 0.25s;
+      }
     &>a{
         padding: 0.3rem;
         margin-right: 2rem;
